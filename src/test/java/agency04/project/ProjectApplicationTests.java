ee41bbb0a10b8b0067f0fc6c08d3a19cd1c0a119
juricaBacic <49362717+juricaBacic@users.noreply.github.com>
2021-02-12 19:39:19 +0100

    Add files via upload

---
+++ src/test/java/agency04/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package agency04.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
